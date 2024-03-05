c062fb3d2b3e638757b8fbf195522aa0ff5d6b64
LaLaYan <>
2024-03-05 11:13:51 +0800

    modify  - fix error

--- app/src/main/java/com/airport/demo/ui/currency/CurrencyViewModel.kt
+++ app/src/main/java/com/airport/demo/ui/currency/CurrencyViewModel.kt
@@ -17,7 +17,7 @@ class CurrencyViewModel(val repo: CurrencyRepo) : ViewModel() {
     val onMessageError: LiveData<Any> = _onMessageError
     private var basePrice: Double = 1000.0
 
-    fun requestExchange(baseCurrency: String = "USD", currencies: String = "EUR,USD,JPY,KRW,CNY,HKD,USD") {
+    fun requestExchange(baseCurrency: String = "USD", currencies: String = "EUR,USD,JPY,KRW,CNY,HKD") {
         repo.fetchCurrencies(baseCurrency,currencies,basePrice,object :OperationCallback<Any>{
             override fun onSuccess(data: Any?) {
                data as ExchangeEntity
